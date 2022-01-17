0861d1024b3f75832b65a31d0d64b09cf319daf1
1 <lukanovs69@gmail.com>
2022-01-17 14:22:31 +0300

    Code refactoring 2 examples one ptichii second not ptichii

--- New.js
+++ New.js
@@ -1,18 +1,14 @@
-
-
-Number.random1 = function(min, max, precision) {
-    var random = Math.random() * (max - min) + min;
+// random number with given precision птичий метод
+  const randomFloat = (min, max, precision) => {
+    const random = Math.random() * (max - min) + min;
     return random.toFixed(precision);
 }
-console.log(Number.random1(2, 10, 2))
+console.log(randomFloat(2, 10, 2))
 
 
-Number.random = function(minimum, maximum) {
-    minimum = minimum === undefined ? 0 : minimum;
-    maximum = maximum === undefined ? 9007199254740992 : maximum;
+// random number without given precision правельный
+    const randomInt = (minimum, maximum) => Math.floor(Math.random() * (maximum - minimum) + minimum)
+console.log(randomInt(2, 10))
 
-    var random = Math.random() * (maximum - minimum) + minimum;
 
-    return Math.floor(random);
-}
-console.log(Number.random(2, 10))
+
